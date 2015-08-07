84a120e2d95a0150c2052dbe2478ad33576822eb
jarontai <jaroncn@gmail.com>
2015-08-07 22:56:13 +0800

    Skelton code

---
+++ lib/io_extra.dart
@@ -0,0 +1,12 @@
+library io_extra;
+
+import 'dart:io';
+import 'dart:async';
+
+Future<bool> copy(String soure, String target, [bool recursive = false]) {
+
+}
+
+bool copySync(String soure, String target, [bool recursive = false]) {
+
+}
